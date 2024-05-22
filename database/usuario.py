8202c0b8c37a3aafda34b460a7db80f199e82027
Matheus Lima <int.matheus.lima@gmail.com>
2024-05-22 20:52:12 -0300

    add: banco de dados fake para teste

---
+++ database/usuario.py
@@ -0,0 +1,6 @@
+USUARIO = [
+    {"id":1, "nome": "matheus", "email":"matheus.lima@teste.com", "senha":"12345678"},
+    {"id":3, "nome": "adriana", "email":"adriana.lima@teste.com", "senha":"adriana123"},
+    {"id":4, "nome": "samuel", "email":"samuel.lima@teste.com", "senha":"samuel123"},
+    {"id":5, "nome": "vitoria", "email":"vitoria.lima@teste.com", "senha":"vitoria123"},
+]
